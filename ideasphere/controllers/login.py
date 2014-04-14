1f7c80897e9bf3494be3aed6540dee90dc84e318
petrushev <b.petrushev@gmail.com>
2014-04-15 00:00:20 +0200

    Added FB login; Added `addmission/addproblem` for admin users

--- ideasphere/controllers/login.py
+++ ideasphere/controllers/login.py
@@ -3,7 +3,7 @@ from json import loads
 from sys import stderr
 
 import requests
-from werkzeug.urls import url_encode
+from werkzeug.urls import url_encode, url_quote, url_decode
 from werkzeug.utils import redirect
 
 from ideasphere.db.models import User
@@ -13,6 +13,11 @@ G = {
     'client_secret': environ['G_SECRET'],
     'token_url': 'https://accounts.google.com/o/oauth2/auth',
     'access_url': 'https://accounts.google.com/o/oauth2/token'
+}
+
+FB = {
+    'consumer_key': environ['FB_CONSUMER_KEY'],
+    'consumer_secret': environ['FB_CONSUMER_SECRET']
 }
 
 def logout(request):
@@ -62,5 +67,65 @@ def g_callback(request):
     u.img_url = content.get('picture')
 
     request.client_session['login'] = ['gmail', service_id]
+
+    return redirect('/' + original_url)
+
+
+
+
+def fb_request(request, original_url):
+    original_url = request.host_url + 'login/callback/fb/' + url_quote(original_url)
+    uri = 'https://www.facebook.com/dialog/oauth/?' + \
+              url_encode({'client_id': FB['consumer_key'],
+                          'redirect_uri': original_url})
+    return redirect(uri)
+
+def fb_callback(request, original_url):
+    if 'code' not in request.args:
+        return redirect('/?msg=2')
+
+    code = request.args['code']
+    callback_uri = request.host_url + 'login/callback/fb/' + url_quote(original_url)
+
+    q = requests.get('https://graph.facebook.com/oauth/access_token?' + \
+                     url_encode({'client_id': FB['consumer_key'],
+                                 'redirect_uri': url_quote(callback_uri),
+                                 'client_secret': FB['consumer_secret'],
+                                 'code': code}))
+
+    if q.status_code != 200:
+        return redirect('/?msg=2')
+
+    content = url_decode(q.content)
+    access_token = content['access_token']
+
+    q = requests.get("https://graph.facebook.com/me?access_token=" + access_token)
+
+    if q.status_code != 200:
+        return redirect('/?msg=2')
+
+    userdata = loads(q.content)
+
+    profile_id = userdata['id']
+    email = userdata.get('email')
+    fullname = userdata['name']
+
+    img_url = None
+    try:
+        q = requests.get("https://graph.facebook.com/%s/picture?redirect=0" % profile_id)
+        if q.status_code == 200:
+            userdata = loads(q.content)['data']
+            img_url = userdata['url']
+
+    except Exception, exc:
+        stderr.write('fb profile pic fetch error: ' + str(exc))
+
+    # save userinfo by id
+    u = User.save_fb_data(request.session, profile_id, fullname, email)
+    u.img_url = img_url
+    request.session.flush()
+
+    # set redirect with cookie
+    request.client_session['login'] = ['fb', profile_id]
 
     return redirect('/' + original_url)
