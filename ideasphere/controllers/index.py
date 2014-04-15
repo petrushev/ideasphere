6cad69e5576c7f0af8dd6a7404035af292222458
petrushev <b.petrushev@gmail.com>
2014-04-15 12:33:22 +0200

    rewamped index page

--- ideasphere/controllers/index.py
+++ ideasphere/controllers/index.py
@@ -5,7 +5,7 @@ GOOGLE_SITE_VERIFICATION = environ['GOOGLE_SITE_VERIFICATION']
 
 def index(request):
     missions = request.session.query(Mission)\
-                      .order_by(Mission.created.desc()).limit(10).all()
+                      .order_by(Mission.created.desc()).all()
 
     return {'google_site_verification': GOOGLE_SITE_VERIFICATION,
             'missions': missions}, \
