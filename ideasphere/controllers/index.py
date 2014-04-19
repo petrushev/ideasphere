2e95ab86a625d3651d402b157612ea6f5e7f13d6
petrushev <b.petrushev@gmail.com>
2014-04-19 14:31:25 +0200

    Added recent problems on index

--- ideasphere/controllers/index.py
+++ ideasphere/controllers/index.py
@@ -1,15 +1,20 @@
 from os import environ
-from ideasphere.db.models import Mission
+from ideasphere.db.models import Mission, Problem
 
 GOOGLE_SITE_VERIFICATION = environ['GOOGLE_SITE_VERIFICATION']
 
 def index(request):
     missions = request.session.query(Mission)\
                       .order_by(Mission.created.desc()).all()
+    problems = request.session.query(Problem)\
+                      .order_by(Problem.created.desc()).limit(6).all()
 
     return {'google_site_verification': GOOGLE_SITE_VERIFICATION,
-            'missions': missions}, \
+            'missions': missions, 'problems': problems}, \
            'index.phtml'
 
 def notfound(request):
     return {}, 'notfound.phtml', 404
+
+def about_us(request):
+    return {}, 'aboutus.phtml', 404
